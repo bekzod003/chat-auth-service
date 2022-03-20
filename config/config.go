10ebe9f3d22e5534c276c358e2697f5011469fb4
bekzod003 <begiobm@gmail.com>
2022-03-21 00:03:05 +0500

    add config

--- config/config.go
+++ config/config.go
@@ -1,4 +1,42 @@
 package config
 
+import (
+	"os"
+
+	"github.com/spf13/cast"
+)
+
 type Config struct {
+	mongoHost     string
+	mongoPort     string
+	mongoDatabase string
+
+	rpcPort string
+}
+
+func (c Config) Load() Config {
+	c.mongoHost = cast.ToString(getOrReturnDefaultValue("MONGO_HOST", "localhost"))
+	c.mongoPort = cast.ToString(getOrReturnDefaultValue("MONGO_PORT", "27017"))
+	c.mongoDatabase = cast.ToString(getOrReturnDefaultValue("MONGO_DATABASE", "auth_service"))
+
+	c.rpcPort = cast.ToString(getOrReturnDefaultValue("RPC_PORT", "8001"))
+	return c
+}
+
+func getOrReturnDefaultValue(key string, defaultValue interface{}) interface{} {
+	_, exists := os.LookupEnv(key)
+
+	if exists {
+		return os.Getenv(key)
+	}
+
+	return defaultValue
+}
+
+func (c Config) GetMongoHost() string {
+	return c.mongoHost
+}
+
+func (c Config) GetMongoPort() string {
+	return c.mongoPort
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/bekzod003/chat-auth-service.git
+
+go 1.17
+
+require github.com/spf13/cast v1.4.1
